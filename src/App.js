fcdbd2bc502988a60b2addfffc55ea5cb459079f
TonyBNK <borisenk-anton@yandex.ru>
2021-11-18 12:49:06 +0300

    Added cors settings.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import {useEffect, useState} from "react";
 import axios from "axios";
+
 
 function App() {
     const [users, setUsers] = useState([]);
 
     useEffect(async () => {
         const response = await axios.get('http://localhost:3010/users');
-        const usersData = [];
-        response.data.forEach(user => usersData.push(user));
-        setUsers(usersData);
+        setUsers(response.data);
     }, []);
 
     return (
