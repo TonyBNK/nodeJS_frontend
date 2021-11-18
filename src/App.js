3ca7e1952cb8656242bab9848b1b6a37d9f13d09
TonyBNK <borisenk-anton@yandex.ru>
2021-11-18 12:36:08 +0300

    Project was created.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [users, setUsers] = useState([]);
+
+    useEffect(async () => {
+        const response = await axios.get('http://localhost:3010/users');
+        const usersData = [];
+        response.data.forEach(user => usersData.push(user));
+        setUsers(usersData);
+    }, []);
+
+    return (
+        <div className="App">
+            {users.map(user => <div>{user.name}</div>)}
+        </div>
+    );
 }
 
 export default App;
