dec2cfd7ba469a803ad59fe8bc6acebb68b7c690
TonyBNK <borisenk-anton@yandex.ru>
2021-11-18 14:57:15 +0300

    Created add new user hardcode functionality.

--- src/App.js
+++ src/App.js
@@ -6,14 +6,26 @@ import axios from "axios";
 function App() {
     const [users, setUsers] = useState([]);
 
-    useEffect(async () => {
+    const getUsers = async () => {
         const response = await axios.get('http://localhost:3010/users');
         setUsers(response.data);
+    }
+
+    const addUserHandler = async () => {
+        await axios.post('http://localhost:3010/users');
+        getUsers();
+    }
+
+    useEffect(() => {
+        getUsers();
     }, []);
 
     return (
         <div className="App">
             {users.map(user => <div>{user.name}</div>)}
+            <button onClick={addUserHandler}>
+                Add user
+            </button>
         </div>
     );
 }
